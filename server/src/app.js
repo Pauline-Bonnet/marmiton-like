d1ca9c9a76e6a6ebe52ceb2b6a1e38c786ea971b
PaulineWildTeacher <pauline.bonnet@wildcodeschool.com>
2025-05-28 15:48:33 +0200

    Episode 2 : ajout de l'ensemble des routes pour USER

--- server/src/app.js
+++ server/src/app.js
@@ -8,47 +8,20 @@ dotenv.config();
 const app = express();
 const PORT = process.env.EXPRESS_PORT || 3000;
 
-// const mockUsers = [
-//     {
-//         user_id: 1,
-//         firstname: "firstname1",
-//         lastname: "lastname1",
-//         pseudo: "1",
-//         email:"firstname1@gmail.com",
-//         password: "password1"
-//     },
-//         {
-//         user_id: 2,
-//         firstname: "firstname2",
-//         lastname: "lastname2",
-//         pseudo: "2",
-//         email:"firstname2@gmail.com",
-//         password: "password2"
-//     },
-//     {
-//         user_id: 3,
-//         firstname: "firstname3",
-//         lastname: "lastname3",
-//         pseudo: "3",
-//         email:"firstname3@gmail.com",
-//         password: "password3"
-//     }
-// ];
+app.use(express.json()); // INDISPENSABLE !!!!
 
 app.use(loggerMiddleware);
+app.use((req, res, next) => {
+    console.log('BODY APP', req.body);
+    next();
+})
 
 app.use('/api/users', userRouter);
 
-// app.get('/api/users/:id', (req, res, next) => {
-//     const userId = Number.parseInt(req.params.id);
-//     const user = mockUsers.find(user => user.id === userId);
-//     if (!user) {
-//         const error = new Error(`le user avec l'id ${userId} n'existe pas`);
-//         error.status = 404;
-//         return next(error);
-//     }
-//     res.status(200).json(user);
-// });
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).json({message: 'Le serveur est dans les choux, sorry'});
+});
 
 app.listen(PORT, () => {
     console.info(`Ce serveur tourne sur le port ${PORT}`);
--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,13 +1,19 @@
 import express from 'express';
-import { getAllUsers } from './../controllers/user.controller.js';
+import { 
+    createUser, 
+    getAllUsers, 
+    getUserById, 
+    updateUser,
+    deleteUser
+
+} from './../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
-// router.get('/:id', getUserById);
-// router.post('/', createUSer);
-// delete
-//put
-//patch
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.patch('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;
